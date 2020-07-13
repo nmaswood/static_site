f2a17fbc312851f44322d0805a79525bc47997b5
Nasr Maswood <nasr@ironcladapp.com>
2020-07-13 10:15:01 -0400

    get prettier working

---
+++ pages/components/Main.tsx
@@ -0,0 +1,3 @@
+export function Main() {
+  return <div> Hello World</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,23 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
 //import './style/main.css';
-import Head from 'next/head'
+import Head from "next/head";
 
-const postsDirectory = path.join(process.cwd(), 'posts')
-
-
+const postsDirectory = path.join(process.cwd(), "posts");
 
 export default function Home() {
-    return (
-        <div className="container">
-            <Head>
-                <title>Nasr Maswood</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <div className="container">
+      <Head>
+        <title>Nasr Maswood</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <main>
-                <h1 className="title">
-                    Nasr Maswood
-                </h1>
-            </main>
-        </div>
-    )
+      <main>
+        <h1 className="title">Nasr Maswood</h1>
+      </main>
+    </div>
+  );
 }
