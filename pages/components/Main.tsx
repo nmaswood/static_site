1323f3df73e85d92af47e9a3e6bd955312f56f46
Nasr.Maswood <nasrmaswood@gmail.com>
2020-07-18 00:32:28 -0400

    Start adding style

--- pages/components/Main.tsx
+++ pages/components/Main.tsx
@@ -1,3 +1,16 @@
 export default function Main() {
-  return <div>Work in Progress! Maybe Later?</div>;
+  return (
+    <div className="Main--container">
+      <header className="Main--title"></header>
+      <div className="Main--about-me">My name is Nasr!</div>
+
+      <label>Some Technical Articles</label>
+      <li className="Main--technical"></li>
+
+      <label>Some Personal Articles</label>
+      <li className="Main--personal"></li>
+
+      <footer></footer>
+    </div>
+  );
 }
