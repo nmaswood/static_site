c24083ec7c45889864584eb0ebbd2a07a3167ecf
Nasr.Maswood <nasrmaswood@gmail.com>
2020-07-21 19:17:00 -0400

    organize pages folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 
-import "./style.css";
-import "./style/Navbar.css";
+import "../style/style.css";
+import "../style/Navbar.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Main from "./components/Main";
-import Navbar from "./components/Navbar";
+import Main from "../components/Main";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
